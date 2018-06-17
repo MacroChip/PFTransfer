356a1d2c1e2b1c0c906debbb995d4a10aad9c8a3
Chip Thien <themacrochip@gmail.com>
2018-06-17 14:35:44 -0500

    Adding electron

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import * as argv from "./args";
 import * as client from "./client";
 import * as server from "./server";
+import {app, BrowserWindow} from 'electron';
+
+let win;
+
+let createWindow = () => {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadFile('static/index.html');
+    win.on('closed', () => {
+        win = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
 
 if (argv.argv._[0] === "server") {
     server.start();
