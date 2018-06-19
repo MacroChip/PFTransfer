5daf1ea90af9e24c120aa615bc89e3d237cb1639
Chip Thien <themacrochip@gmail.com>
2018-06-18 19:01:33 -0500

    Split out the cli which can still be run independantly. Need to change the way the ui runs the cli

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+import * as argv from "./args";
+import * as client from "./client";
+import * as server from "./server";
+
+let main = () => {
+    if (argv.argv._[0] === "server") {
+        server.start();
+    } else if (argv.argv._[0] === "send") {
+        client.send(argv.argv.f, argv.argv.r, argv.argv.s);
+    } else if (argv.argv._[0] === "receive") {
+        client.receive(argv.argv.f, argv.argv.i, argv.argv.s);
+    }
+};
+
+main(); //for when the cli is run
+
+export { //for when the cli is used in the gui
+    main
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import * as argv from "./args";
-import * as client from "./client";
-import * as server from "./server";
 import {app, BrowserWindow} from 'electron';
 
 let win;
@@ -26,11 +23,3 @@ app.on('activate', () => {
         createWindow();
     }
 });
-
-if (argv.argv._[0] === "server") {
-    server.start();
-} else if (argv.argv._[0] === "send") {
-    client.send(argv.argv.f, argv.argv.r, argv.argv.s);
-} else if (argv.argv._[0] === "receive") {
-    client.receive(argv.argv.f, argv.argv.i, argv.argv.s);
-}
