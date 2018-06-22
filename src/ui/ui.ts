64f8f9def6a88d56cbc3e7d302d648b9ca0e5e3d
Chip Thien <themacrochip@gmail.com>
2018-06-22 15:53:53 -0500

    wip. using ui to do commands

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -3,6 +3,16 @@ import * as server from "../server";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("startServer").addEventListener("click", () => {
-        server.start();
+        server.start(); //this works
+    });
+    document.getElementById("send").addEventListener("click", () => {
+        client.send("text.txt", "xdboi", 'http://localhost:8080', (error) => { //this works. tested by doing the receive in the cli and sending here
+            console.log("done", error);
+        });
+    });
+    document.getElementById("receive").addEventListener("click", () => {
+        client.receive("fdsx.txt", "xdboi", 'http://localhost:8080', (error) => { //this does not work. tested by doing send in the cli and receive here.
+            console.log("done", error);
+        });
     });
 });
