098c3d1a811c37b7849de29bb3fb43bf63b12fe5
Chip Thien <themacrochip@gmail.com>
2018-06-27 08:00:48 -0500

    The ui works so remove the comments

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -3,15 +3,15 @@ import * as server from "../server";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("startServer").addEventListener("click", () => {
-        server.start(); //this works
+        server.start();
     });
     document.getElementById("send").addEventListener("click", () => {
-        client.send("text.txt", "xdboi", 'http://localhost:8080', (error) => { //this works. tested by doing the receive in the cli and sending here
+        client.send("text.txt", "xdboi", 'http://localhost:8080', (error) => {
             console.log("done", error);
         });
     });
     document.getElementById("receive").addEventListener("click", () => {
-        client.receive("fdsx.txt", "xdboi", 'http://localhost:8080', (error) => { //this does not work. tested by doing send in the cli and receive here.
+        client.receive("fdsx.txt", "xdboi", 'http://localhost:8080', (error) => {
             console.log("done", error);
         });
     });
